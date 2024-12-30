5a3c82f3ddc347025e56c345a933064c490850cf
Sandhan Meher <118551367+SandhanMeher@users.noreply.github.com>
2024-12-30 15:19:45 +0530

    Update DeveloperInfo.jsx

--- src/Components/Information/DeveloperInfo.jsx
+++ src/Components/Information/DeveloperInfo.jsx
@@ -6,7 +6,7 @@ export default function DeveloperInfo() {
             <h1 className='text-2xl font-bold'>Developer Information</h1>
             <hr className='my-4' />
             <ul className='list-disc pl-5'>
-                <li><strong>Name:</strong> John Doe</li>
+                <li><strong>Name:</strong>Sandhan Meher</li>
                 <li><strong>GitHub ID:</strong> <a href="https://github.com/SandhanMeher" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">SandhanMeher <span role="img" aria-label="GitHub">🐱</span></a></li>
                 <li><strong>LinkedIn ID:</strong> <a href="https://www.linkedin.com/in/sandhan-meher-82b666225/" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">linkedin.com/sandhan-mehere <span role="img" aria-label="LinkedIn">🔗</span></a></li>
                 <li><strong>Twitter ID:</strong> <a href="#" target="_blank" rel="noopener noreferrer" className="hover:text-yellow-300">@Sandhan  <span role="img" aria-label="Twitter">🐦</span></a></li>
